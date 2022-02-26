faffc756f455e68874d0f68ddc48a22d5b5859b5
Suja Tanvir Ahmed <30548517+SujaTanvirAhmed@users.noreply.github.com>
2022-02-26 23:54:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState, useRef } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("No error!");
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+
+  function handleGetUsers() {
+    axios.get("http://localhost:5000/users")
+      .then(response => setUsers(response.data))
+      .catch(err => setError(err.message));
+  }
+
+  function handlePost() {
+    const user = { userName: nameRef.current.value, userEmail: emailRef.current.value };
+    axios.post("http://localhost:5000/users", user)
+      .then(response => {
+        console.log(response.data);
+        nameRef.current.value = "";
+        emailRef.current.value = "";
+      })
+      .catch(() => {
+        console.log("An error occured.");
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello there!</h1>
+      <p>{error}</p>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+      <button onClick={handleGetUsers}>Get Users</button>
+      <br />
+      <input type="text" ref={nameRef} placeholder="Your name" />
+      <input type="email" ref={emailRef} placeholder="Your email" />
+      <button onClick={handlePost}>Post a request</button>
     </div>
   );
 }
